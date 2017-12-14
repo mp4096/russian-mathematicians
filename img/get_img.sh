7eb2a71c95c3d556fe95c9cd068fe13d15b7aa9e
Mikhail Pak <mikhail.pak@tum.de>
2017-12-14 13:43:53 +0100

    Finish Chebyshev

--- img/get_img.sh
+++ img/get_img.sh
@@ -3,3 +3,9 @@
 curl \
   https://upload.wikimedia.org/wikipedia/commons/d/d3/Pafnuty_Lvovich_Chebyshev.jpg \
   -o tschebyschow.jpg
+curl \
+  http://www.tcheb.ru/.data/mechas/23/pictures/005.jpg \
+  -o fliehkraftregler.jpg
+curl \
+  http://www.tcheb.ru/.data/mechas/16/pictures/001.jpg \
+  -o gehmaschine.jpg
